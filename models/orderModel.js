36671d2ae0dceb8d7ee8b12ddf6120a8b176512c
Gabehaus <Gabehaus@gmail.com>
2021-05-07 15:23:25 -0700

    added state to address

--- models/orderModel.js
+++ models/orderModel.js
@@ -24,6 +24,7 @@ const orderSchema = mongoose.Schema(
       recipient: { type: String, required: false },
       address: { type: String, required: true },
       city: { type: String, required: true },
+      state: { type: String, required: true },
       postalCode: { type: String, required: true },
       country: { type: String, required: true }
     },
