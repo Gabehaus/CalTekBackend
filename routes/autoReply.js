a7123b67b942a5833af9ef1d0ea5aae18b9aaf5d
Gabehaus <Gabehaus@gmail.com>
2021-05-07 17:10:18 -0700

    added protect and admin middleware to autoReply route

--- routes/autoReply.js
+++ routes/autoReply.js
@@ -3,6 +3,6 @@ const router = express.Router()
 import { protect, admin } from "../middleware/authMiddleware.js"
 import { deliveryAutoReply } from "../controllers/autoReplyController.js"
 
-router.route("/").post(deliveryAutoReply)
+router.route("/").post(protect, admin, deliveryAutoReply)
 
 export default router
